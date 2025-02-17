c0c64e6158a4c5f982fdaf360136db0e497fcf3e
Mewanprantijwala <mewanchhipa0@gmail.com>
2025-02-17 12:35:03 +0530

    event handler

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import ClassProps from "./Classcompnent/ClassProps";
+import About from "./pages/About";
+import ClassCounter from "./Classcompnent/ClassCounter"
+import Counter from "./Hook/Counter.jsx";
+import Singlefunctionform from "./HandelForm/Singlefunctionform.jsx"
+
+
+const App = () => {
+    return (
+        <>
+            {/* <ClassProps /> */}
+            {/* <About /> */}
+            {/* <ClassCounter/>
+            <Counter/>   */}
+        <Singlefunctionform/>
+        </>
+    )
+}
+export default App
